c8c4fbb244bfb095732a49b74e3e2c071b63a03c
Daniel Siebert <danielsiebert@Daniels-MacBook-Pro.local>
2016-05-04 11:48:47 -0400

    Addition of very simple error handling on service layer.

--- app/home/post.service.ts
+++ app/home/post.service.ts
@@ -26,7 +26,7 @@ export class PostService {
     private _scope = 'read'; 
     
     constructor(private _http: Http){
-        
+        this.authorizeApp();
     }
     
     authorizeApp(): void {
@@ -34,6 +34,7 @@ export class PostService {
                        this._clientId + '&response_type=' + this._responseType +
                        '&state=' + this._state + '&redirect_uri=' + this._redirectUri + 
                        '&duration=' + this._duration + '&scope=' + this._scope;
+        console.log('Authorize Url: ' + url);
                        
         // Post to this URL, get the 'code' to retrieve the token
         
@@ -44,11 +45,12 @@ export class PostService {
         var jsonBody = 'grant_type=' + this._grantType + 
                             '&code=' + _code + 
                             '&redirect_uri=' + this._redirectUri; 
+        console.log('Post Body: ' + jsonBody);
                             
         var postResponse = this._http.post(this._accessTokenUrl, jsonBody)
                     .map((response: Response) => <IAuthenticate[]>response.json());
                     
-        console.log(postResponse);
+        console.log(postResponse[0]);
                     
         // Check out to make sure the call worked
         return true;
@@ -58,27 +60,38 @@ export class PostService {
         var url = this._redditAPIBaseUrl + this._redditHotUrl;
         
         return this._http.get(url)
-            .map((response: Response) => <IPost[]>response.json());
+            .map((response: Response) => <IPost[]>response.json())
+            .catch(this.handleError);
     }
     
     getNewPosts(): Observable<IPost[]>{
         var url = this._redditAPIBaseUrl + this._redditNewUrl;
         
         return this._http.get(url)
-            .map((response: Response) => <IPost[]>response.json());
+            .map((response: Response) => <IPost[]>response.json())
+            .catch(this.handleError);;
     }
     
     getTopPosts(): Observable<IPost[]>{
         var url = this._redditAPIBaseUrl + this._redditTopUrl;
         
         return this._http.get(url)
-            .map((response: Response) => <IPost[]>response.json());
+            .map((response: Response) => <IPost[]>response.json())
+            .catch(this.handleError);;
     }
     
     getRandomPost(): Observable<IPost[]>{
         var url = this._redditAPIBaseUrl + this._redditRandomUrl;
         
         return this._http.get(url)
-            .map((response: Response) => <IPost[]>response.json());
+            .map((response: Response) => <IPost[]>response.json())
+            .catch(this.handleError);;
+    }
+    
+    private handleError(error: Response){
+        // This is where service layer exceptions will be managed.
+        console.error(error);
+        
+        return Observable.throw(error.json().error || 'Server Error');
     }
 }
