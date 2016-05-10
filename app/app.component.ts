4001cea0fc2b2cec9af8a1b3dd72392751474e4c
Daniel Siebert <danielsiebert@Daniels-MacBook-Pro.local>
2016-05-10 09:37:14 -0400

    Creation of new landing page for application. This page will be used to callout to Reddit to negotiate the OAuth2 credentials. Also includes the skeleton for routing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,29 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx';
-import {PostListComponent} from './home/post-list.component';
-import {PostService} from './home/post.service';
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+
+import { PostListComponent } from './home/post-list.component';
+import { PostService } from './home/post.service';
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 @Component({
     selector: 'gettit-app',
     template: `
     <h1>{{pageTitle}}</h1>
-    <gettit-postList>Loading Posts...</gettit-postList>
+    <gettit-authentication>Loading...</gettit-authentication>
     `,
-    directives: [PostListComponent],
+    directives: [PostListComponent,
+                 AuthenticationComponent,
+                 ROUTER_DIRECTIVES],
     providers: [PostService,
-                HTTP_PROVIDERS]
+                HTTP_PROVIDERS,
+                ROUTER_PROVIDERS]
 })
+@RouteConfig([
+    { path: "/authenticate", name : "Authenicate", component: AuthenticationComponent, useAsDefault: true },
+    { path: "/home", name: "Home", component: PostListComponent }
+])
 export class AppComponent { 
     pageTitle: string = 'Gettit: An Angular 2 Project cosuming the Reddit API';
 }
---
+++ app/authentication/authentication.component.ts
@@ -0,0 +1,18 @@
+import { Component } from 'angular2/core';
+import { ROUTER_PROVIDERS, RouteConfig, ROUTER_DIRECTIVES } from 'angular2/router';
+
+import { AuthenticationService } from './authentication.service';
+
+@Component({
+    selector: 'gettit-authentication',
+    templateUrl: 'app/authentication/authentication.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS,
+                AuthenticationService]
+})
+export class AuthenticationComponent {
+    
+    constructor(private _authenticationService: AuthenticationService){
+        console.log("Loaded AuthenticationComponent");
+    }
+}
